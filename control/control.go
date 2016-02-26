b5e11309d5e2cf0ace7e72c3fa6a8a2ffb308351
ziel <ziel@traveling>
2016-02-26 15:08:04 -0500

    Refactor controller to manage cleanup funcs

--- control/control.go
+++ control/control.go
@@ -14,54 +14,105 @@ import (
 
 // todo: docs
 type state struct {
-	model model.Model
-	view  view.View
-	wg    sync.WaitGroup
+	model     model.Model
+	view      view.View
+	waitGroup sync.WaitGroup
+	cleaners  []func()
 }
 
 // todo: docs
 var controller *state
 
 // todo: docs
+func newController() *state {
+	return &state{
+		cleaners: make([]func(), 0),
+	}
+}
+
+// todo: docs
 func Init(paths []string) error {
-	cModel, err := model.Factory(paths)
-	if err != nil {
+	controller = newController()
+	defer controller.cleanup()
+
+	if err := controller.initModel(paths); err != nil {
 		return err
 	}
 
-	defer func() {
-		if err := cModel.Close(); err != nil {
-			log.Println(err)
-		}
-	}()
-
-	cView, err := view.Factory(paths)
-	if err != nil {
+	if err := controller.initView(paths); err != nil {
 		return err
 	}
 
-	controller = &state{
-		model: cModel,
-		view:  cView,
+	if err := controller.initTermbox(); err != nil {
+		return err
 	}
 
 	return controller.loop()
 }
 
 // todo: docs
+func (s *state) initModel(paths []string) error {
+	mdl, err := model.Factory(paths)
+	if err != nil {
+		return err
+	}
+
+	s.addCleaner(func() {
+		if err := mdl.Close(); err != nil {
+			log.Println(err)
+		}
+	})
+
+	s.model = mdl
+	return nil
+}
+
+// todo: docs
+func (s *state) initView(paths []string) error {
+	viw, err := view.Factory(paths)
+
+	if err != nil {
+		return err
+	}
+
+	s.view = viw
+	return nil
+}
+
+// todo: docs
+func (s *state) addCleaner(fn func()) {
+	s.cleaners = append(s.cleaners, fn)
+}
+
+// todo: docs
+func (s *state) cleanup() {
+	for _, fn := range s.cleaners {
+		fn()
+	}
+}
+
+// todo: docs
 func (s *state) loop() error {
+	events := s.eventProducer()
+	result := s.eventConsumer(events)
+
+	s.waitGroup.Wait()
+	return <-result
+}
+
+// todo: docs
+func (s *state) initTermbox() error {
 	if err := termbox.Init(); err != nil {
 		return err
 	}
 
-	defer termbox.Close()
+	s.addCleaner(termbox.Close)
+
 	termbox.SetInputMode(termbox.InputAlt)
+	termbox.Flush()
 
-	events := s.eventProducer()
-	result := s.eventConsumer(events)
-
-	s.wg.Wait()
-	return <-result
+	w, h := termbox.Size()
+	return s.view.Update(w, h)
 }
 
 // todo: docs
@@ -69,10 +120,10 @@ func (s *state) eventProducer() <-chan termbox.Event {
 	const bufsize int = 10
 	events := make(chan termbox.Event, bufsize)
 
-	s.wg.Add(1)
+	s.waitGroup.Add(1)
 
 	go func() {
-		defer s.wg.Done()
+		defer s.waitGroup.Done()
 
 		for {
 			ev := termbox.PollEvent()
@@ -88,25 +139,23 @@ func (s *state) eventProducer() <-chan termbox.Event {
 
 // todo: docs
 func (s *state) eventConsumer(events <-chan termbox.Event) <-chan error {
-	result := make(chan error)
+	result := make(chan error, 1)
 
-	s.wg.Add(1)
+	s.waitGroup.Add(1)
 
 	go func() {
-		defer s.wg.Done()
+		defer s.waitGroup.Done()
 
 		for {
 			s.view.Draw()
 
 			if err := s.handle(<-events); err != nil {
-				switch err {
-				case errors.Quit:
-					close(result)
-					termbox.Interrupt()
-
-				default:
+				if err != errors.Quit {
 					result <- err
 				}
+
+				close(result)
+				termbox.Interrupt()
 
 				return
 			}
